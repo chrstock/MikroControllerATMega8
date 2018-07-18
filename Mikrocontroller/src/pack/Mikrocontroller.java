544f1e8bba05f44257836f3c0cdc3d4a67e5ce83
chrstock <christian.a.stock@capgemini.com>
2018-07-18 10:17:35 +0200

    Master Commit

--- Mikrocontroller/src/pack/Mikrocontroller.java
+++ Mikrocontroller/src/pack/Mikrocontroller.java
@@ -1,78 +1,90 @@
 package pack;
 
+/**
+ * Class to calculate Mikrocontroller - Image
+ * 
+ * @author chrstock
+ *
+ */
 public class Mikrocontroller {
-	private char[][] ledDisplay = new char[10][12];
-	private String message;
+  private char[][] ledDisplay = new char[10][12];
 
-	public Mikrocontroller(String message) {
-		this.message = message;
-		
-		for(int i = 0; i <= message.length() - 7;i+=7 ) {
-			translateASCII(message.substring(i, i+7));
-			printAnzeige();
-		}
-	}
-	
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	
-	public String getMessage() {
-		return message;
-	}
+  private String message;
 
-	public void printAnzeige() {
+  public Mikrocontroller(String message) {
 
-		for (char[] ledRow : ledDisplay) {
-			for (char led : ledRow) {
-				System.out.print(led + " ");
-			}
-			System.out.println();
-		}
-		System.out.println();
-	}
+    this.message = message;
+    //test message
+    for (int i = 0; i <= message.length() - 7; i += 7) {
+      translateASCII(message.substring(i, i + 7));
+      printAnzeige();
+    }
+  }
 
-	private void setAllBitLampsOff() {
-		for (int i = 0; i < ledDisplay.length; i++) {
-			for (int j = 0; j < ledDisplay[i].length; j++) {
-				ledDisplay[i][j] = 'x';
-			}
-		}
-		ledDisplay[0][0] = '1';
-		ledDisplay[0][11] = '1';
-		ledDisplay[9][0] = '1';
-		ledDisplay[9][11] = '1';
-	}
+  public void setMessage(String message) {
 
-	private void translateASCII(String ascii) {
-		int teiler;
-		char letter;
-		int value;
+    this.message = message;
+  }
 
-		setAllBitLampsOff();
+  public String getMessage() {
 
-		StringBuilder newAscii = new StringBuilder(ascii);
-		for (int i = 0; i < 8 - ascii.length(); i++) {
-			newAscii.append(" ");
-		}
+    return this.message;
+  }
 
-		ascii = newAscii.toString();
+  public void printAnzeige() {
 
-		for (int i = 0; i < 8; i++) {
-			teiler = 128;
-			letter = ascii.charAt(i);
-			value = (int) letter;
+    for (char[] ledRow : this.ledDisplay) {
+      for (char led : ledRow) {
+        System.out.print(led + " ");
+      }
+      System.out.println();
+    }
+    System.out.println();
+  }
 
-			for (int j = 2; j < 10; j++) {
-				if (value / teiler == 1) {
-					ledDisplay[i + 1][j] = '1';
-					value = value % teiler;
-				} else {
-					ledDisplay[i + 1][j] = '0';
-				}
-				teiler /= 2;
-			}
+  private void setAllBitLampsOff() {
 
-		}
-	}
+    for (int i = 0; i < this.ledDisplay.length; i++) {
+      for (int j = 0; j < this.ledDisplay[i].length; j++) {
+        this.ledDisplay[i][j] = 'x';
+      }
+    }
+    this.ledDisplay[0][0] = '1';
+    this.ledDisplay[0][11] = '1';
+    this.ledDisplay[9][0] = '1';
+    this.ledDisplay[9][11] = '1';
+  }
+
+  private void translateASCII(String ascii) {
+
+    int teiler;
+    char letter;
+    int value;
+
+    setAllBitLampsOff();
+
+    StringBuilder newAscii = new StringBuilder(ascii);
+    for (int i = 0; i < 8 - ascii.length(); i++) {
+      newAscii.append(" ");
+    }
+
+    ascii = newAscii.toString();
+
+    for (int i = 0; i < 8; i++) {
+      teiler = 128;
+      letter = ascii.charAt(i);
+      value = letter;
+
+      for (int j = 2; j < 10; j++) {
+        if (value / teiler == 1) {
+          this.ledDisplay[i + 1][j] = '1';
+          value = value % teiler;
+        } else {
+          this.ledDisplay[i + 1][j] = '0';
+        }
+        teiler /= 2;
+      }
+
+    }
+  }
 }
